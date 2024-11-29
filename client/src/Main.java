2f092d30c6b64723a4c7012cd55c99c771a6b9e9
Tan <tantr.us91@gmail.com>
2024-11-29 11:40:56 -0800

    Minor update. Remove unuse package.

--- client/src/Main.java
+++ client/src/Main.java
@@ -1,7 +1,8 @@
-import org.mindrot.jbcrypt.BCrypt;
 import view.LoginView;
 
 import javax.swing.*;
+
+import static javax.swing.SwingUtilities.*;
 
 public class Main {
     public static void main(String [] args) {
